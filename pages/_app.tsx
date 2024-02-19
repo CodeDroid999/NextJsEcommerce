b774076600a7b1a4186a2f1506396de99c618008
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-20 02:53:44 +0300

    SKENGA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,12 @@ import { Toaster } from 'react-hot-toast'
 import '../styles/custom.css'
 import { AuthContextProvider } from '@/context/AuthContext'
 
-
-export default function App({
-
-
-
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <AuthContextProvider>
         <Toaster position="bottom-center" />
-
+        <Component {...pageProps} />
       </AuthContextProvider>
     </>
   )
