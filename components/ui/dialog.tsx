9a9b68ce7daf5c13af66d8deea80e95a2b135a3e
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-22 15:56:23 +0300

    dpp

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -11,10 +11,11 @@ const Dialog = DialogPrimitive.Root
 const DialogTrigger = DialogPrimitive.Trigger
 
 const DialogPortal = ({
+  className,
   children,
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal {...props}>
+  <DialogPrimitive.Portal className={cn(className)} {...props}>
     <div className="fixed inset-0 z-50 flex items-start justify-center sm:items-center">
       {children}
     </div>
@@ -114,3 +115,14 @@ const DialogDescription = React.forwardRef<
     {...props}
   />
 ))
+DialogDescription.displayName = DialogPrimitive.Description.displayName
+
+export {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+}
